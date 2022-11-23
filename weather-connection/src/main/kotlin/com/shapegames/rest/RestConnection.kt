84a2a85b5a7d80f8fb74848d38d9cfdf05d7810b
Milan Cop <milan.cop@infodyn.eu>
2022-11-23 13:48:18 +0100

    introduced synchronous get rest call

---
+++ weather-connection/src/main/kotlin/com/shapegames/rest/RestConnection.kt
@@ -0,0 +1,25 @@
+package com.shapegames.rest
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+class RestConnection {
+
+    private val client:OkHttpClient = OkHttpClient()
+
+    fun syncGet(url:String):String {
+        val request = Request.Builder()
+            .get()
+            .url(url)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            //TODO handle exceptions
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            println(response.body!!.string())
+            return  response.body?.let { return it.string() } ?: throw IOException("Malformed response") //TODO handle exception
+        }
+    }
+}
